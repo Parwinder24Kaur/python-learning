e40b9060a05aabeddf768604e64369c895aedb15
Parwinder24Kaur <125437138+Parwinder24Kaur@users.noreply.github.com>
2023-02-24 12:48:00 +0530

    Add files via upload

---
+++ first assignment (1).py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#WAP to read Employee Data from the keyboard and print data
+employee_no = int(input("Employee no:  "))
+employee_name=input("Employee Name: ")
+employee_Salary=float(input("Employee Salary: "))
+employee_Address=input("Employee Address: ")
+employee_is_married=input("True or False")
+
+if employee_is_married =="True":
+    print('Employee is married ')
+if employee_is_married == "False":
+     print('Employee is not married' )
+
+    
+
+
+
+
+# In[3]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
